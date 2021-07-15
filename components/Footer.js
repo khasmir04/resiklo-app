160cd750781f4be4eb7525ce6c6954938db621d1
Khasmir Jhon Caluscusao <61497334+khasmir04@users.noreply.github.com>
2021-07-15 22:02:47 +0800

    Fixing footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center w-full h-24 border-t">
-      <a
-        className="flex items-center justify-center"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Our Sponsors{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-      </a>
+    <footer className="bg-primary-dark text-white flex items-center justify-center w-full h-24 border-t">
+      <div className="flex flex-wrap items-center">
+        <div className="w-full lg:w-1/2"></div>
+        <div className="w-full lg:w-1/2"></div>
+      </div>
     </footer>
   )
 }
