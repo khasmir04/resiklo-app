e43e7d834d33a62b6842bcdcfed1e6a5c5f42ce4
Khasmir Jhon Caluscusao <61497334+khasmir04@users.noreply.github.com>
2021-07-15 14:37:10 +0800

    Edited index page

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,82 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+  const [navbar, setNavbar] = useState(true);
+
+  const toggleNavbar = () => {
+    setNavbar(!navbar);
+  }
+
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Kalakal</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center pt-20">
+
+        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            <a className="text-white no-underline hover:text-white hover:no-underline" href="#">
+              <span className="text-2xl pl-2"><i className="em em-grinning" /> Kalakal</span>
+            </a>
+          </div>
+          <div className="block lg:hidden">
+            <button
+              onClick={() => toggleNavbar()}
+              id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
+              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+            </button>
+          </div>
+          <div
+
+            className={`w-full flex-grow lg:flex lg:items-center lg:w-auto pt-6 lg:pt-0 ` + (navbar ? 'hidden' : null)}
+            id="nav-content">
+            <ul className="list-reset lg:flex justify-end flex-1 items-center">
+              <li className="mr-3">
+                <a className="inline-block py-2 px-4 text-white no-underline" href="#">Active</a>
+              </li>
+              <li className="mr-3">
+                <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+              </li>
+              <li className="mr-3">
+                <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+              </li>
+              <li className="mr-3">
+                <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Kalakal
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+        <div className="flex flex-col flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           <a
             href="https://nextjs.org/docs"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-4xl text-center font-bold">Manage</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+              Manage your wastes.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/docs"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-4xl text-center font-bold">Buy</h3>
+            <p className="mt-4 text-xl">
+              Buy waste from other users.
             </p>
           </a>
 
@@ -38,31 +84,12 @@ export default function Home() {
             href="https://nextjs.org/learn"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-4xl text-center font-bold">Sell</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Earn money by selling wastes.
             </p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -73,7 +100,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Our Sponsors{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
