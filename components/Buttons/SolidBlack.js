b9fa4bec4da9f43ebd92aed34c826c69e05e4161
Janiel Banasihan <janimargaret09@gmail.com>
2021-07-15 23:05:01 +0800

    Added button components, fixed hover effects, and added Footer.js

---
+++ components/Buttons/SolidBlack.js
@@ -0,0 +1,12 @@
+const SolidBlack = (props) => {
+  return (
+    <a
+      href={props.link}
+      className="inline-flex text-white bg-black border border-black py-2 px-6 focus:outline-none hover:bg-transparent hover:text-black rounded-[20px] text-lg font-medium uppercase"
+    >
+      {props.children}
+    </a>
+  );
+};
+
+export default SolidBlack;
---
+++ components/Buttons/SolidGreen.js
@@ -0,0 +1,12 @@
+const SolidGreen = (props) => {
+  return (
+    <a
+      href={props.link}
+      className="inline-flex text-white bg-secondary-light border border-secondary-light py-2 px-6 focus:outline-none hover:bg-transparent hover:text-white hover:border-white rounded-[20px] text-lg font-medium uppercase"
+    >
+      {props.children}
+    </a>
+  );
+};
+
+export default SolidGreen;
