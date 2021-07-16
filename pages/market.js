7cbb705a1129c4b1fe5b3973a26186d4f8e4c0a8
Janiel Banasihan <janimargaret09@gmail.com>
2021-07-16 12:55:41 +0800

    Added market.js

--- pages/market.js
+++ pages/market.js
@@ -5,19 +5,40 @@ const market = () => {
   return (
     <div id="market">
       <LayoutHome title="Market">
-        <div className="flex flex-col items-center justify-center p-32">
-          <h1 className="text-primary-dark font-bold text-7xl font-rubik">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-primary-dark font-bold text-7xl font-rubik mb-10">
             Welcome to the Market!
           </h1>
-          <p className="text-black text-lg font-dmsans text-center">
+          <p className="text-black text-lg font-dmsans text-center max-w-[900px]">
             To start, press one of the options below. Lorem ipsum dolor sit
             amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
             ut labore et dolore magna aliqua. Ut Lorem ipsum dolor sit amet,
             consectetur adipiscing elit, sed do eiusmod.
           </p>
-        </div>
-        <div className="flex flex-row">
-          <div className="bg-white rounded"></div>
+          <div className="flex flex-row mt-24">
+            <div className="shadow bg-white rounded-2xl border-2 border-secondary py-6 px-7 mr-12">
+              <div className="flex">
+                <img src="" alt="tool-icon" />
+                <div className="flex flex-col text-secondary-light ml-4">
+                  <h3 className="font-medium text-2xl font-rubik">
+                    Estimate Tool
+                  </h3>
+                  <p className="text-lg font-dmsans">description</p>
+                </div>
+              </div>
+            </div>
+            <div className="shadow bg-white rounded-2xl border-2 border-secondary py-6 px-7">
+              <div className="flex">
+                <img src="" alt="shop-icon" />
+                <div className="flex flex-col text-secondary-light ml-4">
+                  <h3 className="font-medium text-2xl font-rubik">
+                    Explore Junk Shops
+                  </h3>
+                  <p className="text-lg font-dmsans">description</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </LayoutHome>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: {
-          light: '#45BF55',
-          DEFAULT: '#45BF55',
-          dark: '#044d29',
+          light: "#45BF55",
+          DEFAULT: "#45BF55",
+          dark: "#044d29",
         },
         secondary: {
-          light: '#97ED8A',
-          DEFAULT: '#97ED8A',
-          dark: '#97ED8A',
+          light: "#16B039",
+          DEFAULT: "#97ED8A",
+          dark: "#97ED8A",
         },
         tertiary: {
-          light: '#85d7ff',
-          DEFAULT: '#1fb6ff',
-          dark: '#009eeb',
+          light: "#85d7ff",
+          DEFAULT: "#1fb6ff",
+          dark: "#009eeb",
         },
         black: {
-          light: '#00261C',
-          DEFAULT: '#00261C',
-          dark: '#00261C',
+          light: "#00261C",
+          DEFAULT: "#00261C",
+          dark: "#00261C",
         },
       },
       fontFamily: {
-        'rubik': ['Rubik', 'sans-serif'],
-        'dmsans': ['DM Sans', 'sans-serif'],
+        rubik: ["Rubik", "sans-serif"],
+        dmsans: ["DM Sans", "sans-serif"],
       },
     },
   },
@@ -36,4 +36,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
